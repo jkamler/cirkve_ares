0d1b241cc5cebbb99522ce4ba491364cf59341b2
Jara <jaromir.kamler@gmail.com>
2017-02-26 14:15:00 +0100

    working map switching, playing with map projections ...

--- js/maps.js
+++ js/maps.js
@@ -1,28 +1,56 @@
-$("#info_wrapper").hide();
+$( document ).ready(function() {
+  $('#orto').prop( "checked", true );
+  $('#basic').prop( "checked", false );
 
-$('#hide_info_wrapper').click(function(){
   $("#info_wrapper").hide();
+
+  $('#hide_info_wrapper').click(function(){
+    $("#info_wrapper").hide();
+  });
+
+  $("#display").val("Název církve nebo města");
+
+  $("#display").click(function() {
+      $("#display").val("");
+  });
 });
-
-$("#display").val("Název církve nebo města");
-
-$("#display").click(function() {
-    $("#display").val("");
-});
-
 
 //projection S-JTSK
-var projection = new ol.proj.Projection({
+var projectionSJTSK = new ol.proj.Projection({
   code: 'EPSG:5514',
+//  code: 'EPSG:4326',
   units: 'm'
 });
 
-ol.proj.addProjection(projection);
+//projection WGS84
+var projectionWGS84 = new ol.proj.Projection({
+    code: 'EPSG:4326',
+    units: 'm',
+    extent: [-180.0000, -90.0000, 180.0000, 90.0000]
+});
+
+//projection Mercator EPSG:900913
+var projectionMercator = new ol.proj.Projection({
+//    code: 'EPSG:3857',
+    code: 'EPSG:900913',
+    units: 'm',
+    extent: [-7501981,-1304807, 9178903, 7369995]
+//    extent: [-180.0000, -90.0000, 180.0000, 90.0000] //WGS84
+});
+
+
+//register projection at OL
+ol.proj.addProjection(projectionSJTSK);
+ol.proj.addProjection(projectionWGS84);
+ol.proj.addProjection(projectionMercator);
+
 
 //coordinates
 var mousePositionControl = new ol.control.MousePosition({
   coordinateFormat: ol.coordinate.createStringXY(),
-  projection: 'EPSG:5514',
+//  projection: 'EPSG:4326',
+//  projection: 'EPSG:3857', //mercator
+//  projection: projectionSJTSK,
   // comment the following two lines to have the mouse position
   // be placed within the map.
   className: 'custom-mouse-position',
@@ -87,6 +115,8 @@ var vectorCirkve = new ol.layer.Vector({
   title: 'Body',
   source: new ol.source.Vector({
     url: 'http://localhost/cirkve_ares/app/getjson.php?query=',
+//    projection: projectionSJTSK,
+    projection: projectionMercator,
     format: new ol.format.GeoJSON()
   }),
   style: function(feature, resolution) {
@@ -95,14 +125,31 @@ var vectorCirkve = new ol.layer.Vector({
   }
 });
 
+vectorCirkve.transform
+
 var ortofotoWMS = new ol.layer.Tile({
   source: new ol.source.TileWMS({
     url: 'http://geoportal.cuzk.cz/WMS_ORTOFOTO_PUB/WMService.aspx',
     params: {'LAYERS': 'GR_ORTFOTORGB', 'TILED': true},
+    serverType: 'geoserver',
+    projection: projectionMercator //
+  })
+});
+/*
+var basicWMS = new ol.layer.Tile({
+  visible: false,
+  source: new ol.source.TileWMS({
+    url: 'http://geoportal.cuzk.cz/WMS_ZM10_PUB/WMService.aspx',
+    params: {'LAYERS': 'GR_ZM10', 'TILED': true},
     serverType: 'geoserver'
   })
 });
-
+*/
+var basicWMS = new ol.layer.Tile({
+  source: new ol.source.OSM({
+    projection: projectionMercator
+  })
+});
 
 //seting up interaction
 var selectInteraction = new ol.interaction.Select({
@@ -115,19 +162,22 @@ var selectInteraction = new ol.interaction.Select({
   }
 });
 
-
 //basic declarations of map
 var map = new ol.Map({
 controls: ol.control.defaults().extend([mousePositionControl]),
   target: 'map',
   view: new ol.View({
-    center: [-670000, -1080000],
-    zoom: 9,
-    projection: projection
+//    center: [-670000, -1080000], // S-JTSK
+    center: [17, 50], //WGS84
+//    zoom: 9,
+    zoom: 5,
+//    projection: projectionSJTSK
+    projection: projectionMercator
   })
 });
 
 map.addLayer(ortofotoWMS);
+map.addLayer(basicWMS);
 map.addLayer(vectorCirkve);
 map.getInteractions().extend([selectInteraction]);
 
@@ -144,27 +194,45 @@ var displayFeatureInfo = function(pixel) {
     $("#info_wrapper").show();
     var info = [];
     for (var i = 0, ii = features.length; i < ii; ++i) {
-
       info.push('<div class="cirkevniContainer"><div class="cirkevNameContainer">' + features[i].get('Nazev_CPO') + '</div><br><div class="cirkevPropertiesContainer">IČ: ' + features[i].get('ICO') + '<br>Ulice: ' + features[i].get('Nazev_ulice') + ' ' + features[i].get('Cislo_do_adresy') + '<br>Obec: ' + features[i].get('Nazev_obce') + '<br>PSČ: ' + features[i].get('PSC') + '<br>Zřizovatel:<br>' + features[i].get('Zrizovatel_text') + '<br>Zvláštní práva:<br>' + features[i].get('Zvlastni_prava') + '</div></div>');
     }
     container.innerHTML = info.join('<hr>') || '(unknown)';
   } else {
+    $("#info_wrapper").hide();
     container.innerHTML = '&nbsp;';
   }
 };
 
 map.on('click', function(evt) {
-/**/vectorCirkve.setVisible(false);
+//  ortofotoWMS.setVisible(false);
+//  basicWMS.setVisible(true);
+//  WMSlayerGroup.setVisible(false);
   var pixel = evt.pixel;
   displayFeatureInfo(pixel);
 });
 
+
+//switching maps
+$('#controlWrapper').click(function() {
+   if($('#orto').is(':checked')) {
+    ortofotoWMS.setVisible(true);
+    basicWMS.setVisible(false);
+   }
+   if($('#basic').is(':checked')) {
+    ortofotoWMS.setVisible(false);
+    basicWMS.setVisible(true);
+    alert(basicWMS.getProjection());
+   }
+
+});
+
+//dynamic search
 $("#display").keyup(function() {
   var s = new ol.source.Vector({
     url: 'http://localhost/cirkve_ares/app/getjson.php'  + '?query=' + $("#display").val(),
     format: new ol.format.GeoJSON()
   });
-  l = map.getLayers().getArray()[1];
+  l = map.getLayers().getArray()[2];
   l.setSource(s);
 });
 
